a8caad2dca1c350b7aedfb15de109dccc96bf7e1
mozzie <mozzie@localhost.localdomain>
2021-07-23 22:00:44 +0800

    revice chisel hello world

--- hello-world/src/main/scala/Hello.scala
+++ hello-world/src/main/scala/Hello.scala
@@ -1,14 +1,14 @@
 /*
  * This code is a minimal hardware described in Chisel.
  * 
- * Blinking LED: the FPGA version of Hello World
+ * And Gate: the FPGA version of Hello World
  */
 
 import chisel3._
 import chisel3.Driver
 
 /**
- * The blinking LED component.
+ * The And Gate component.
  */
 
 class Hello extends Module {
