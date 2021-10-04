6e11b77c456c1ce49a46fc3aaed9fa46fb67f278
huxuan0307 <532391012@qq.com>
2021-10-04 21:51:30 +0800

    Crossbar: Delete write arbiter in crossbar Data cache connect write port with axi bridge straightly.

--- src/main/scala/Core/CoreTop.scala
+++ src/main/scala/Core/CoreTop.scala
@@ -9,7 +9,7 @@ import chisel3._
 import chisel3.util.ValidIO
 import difftest._
 import Core.Cache.DCache
-import Core.AXI4.{AXI4IO, CROSSBAR_Nto1}
+import Core.AXI4.{AXI4IO, Crossbar}
 
 class SimTopIO extends Bundle {
   val logCtrl = new LogCtrlIO
@@ -30,12 +30,12 @@ class SimTop extends Module {
   val exublock = Module(new ExuBlock)
   val icache = Module(new DCache(cacheNum = 0))
   val dcache = Module(new DCache(cacheNum = 1))
-  val crossbar_xto1 = Module(new CROSSBAR_Nto1(ro_num = 1, rw_num = 1))
+  val crossbar = Module(new Crossbar)
 
-  io.memAXI_0 <> crossbar_xto1.io.out
+  io.memAXI_0 <> crossbar.io.out
 
-  crossbar_xto1.io.in(0) <> icache.io.to_rw
-  crossbar_xto1.io.in(1) <> dcache.io.to_rw
+  crossbar.io.in(0) <> icache.io.to_rw
+  crossbar.io.in(1) <> dcache.io.to_rw
 
 
   icache.io.req <> ifu.io.cachereq
